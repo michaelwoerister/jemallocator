03b90ad1cd73477a70a1951275f729379e2b6ed3
Travis CI User <travis@example.org>
2018-11-10 00:50:27 +0000

    Documentation for alexcrichton/jemallocator@08c2168

---
+++ implementors/core/default/trait.Default.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["jemalloc_sys"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"jemalloc_sys/struct.extent_hooks_s.html\" title=\"struct jemalloc_sys::extent_hooks_s\">extent_hooks_s</a>",synthetic:false,types:["jemalloc_sys::extent_hooks_s"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
